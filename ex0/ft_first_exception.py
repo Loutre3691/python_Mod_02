ebe759cab1c9250599bba06232e4520a692d4c62
Flora De Checchi <flora.dechecchi@gmail.com>
2026-03-12 17:24:19 +0100

    ex0 debut

---
+++ ex0/ft_first_exception.py
@@ -0,0 +1,11 @@
+def check_temperature(temp_str: int) -> None:
+    try
+        temp_str 
+
+def test_temperature_input() -> None:
+
+
+if __name__ == "__main__":
+    temp = int(input("Thanks enter temperature: "))
+    check_temperature(temp)
+
