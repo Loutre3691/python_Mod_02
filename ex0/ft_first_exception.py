68999580d4934b2d1d04a16e949e2496b4428b50
flora <flora.dechecchi@gmailcom>
2026-03-12 22:58:17 +0100

    ex0

--- ex0/ft_first_exception.py
+++ ex0/ft_first_exception.py
@@ -1,11 +1,29 @@
-def check_temperature(temp_str: int) -> None:
-    try
-        temp_str 
+def check_temperature(temp_str: int) -> None:    
+    if temp_str > 40:
+        raise print(f"\033[0;31mError\n\033[0m: {temp_str}°C is too hot for plants (max 40°C")
+    elif temp_str < 0:
+        raise print(f"\033[0;31mError\n\033[0m: {temp_str}°C is too cold for plqnts (min 0°C)")
+    else:
+        print(f"\033[1;36mTemperature {temp_str} is perfect for plant!\033[0m")
 
-def test_temperature_input() -> None:
+def test_temperature_input(temp_str: str) -> None:
+    print(f"Testing temperature: {temp_str}")
+    try:
+       temp = int(temp_str)
+    except:
+        print(f"\033[0;31mError\033[0m: '{temp_str}' is not a valid number")
+        return
+    try:
+        check_temperature(temp_str)
+    except:
+        print("\033[1;31mERROR\n\033[0m")
+        return
 
-
+# def test_temperature_input() -> None:
 if __name__ == "__main__":
-    temp = int(input("Thanks enter temperature: "))
-    check_temperature(temp)
 
+    print("\033[1;32m=== Garden Temperature checker ===\n\033[0m")
+    test = ["25", "abc", "100", "-50"]
+    for t in test:
+        test_temperature_input(t)
+    print("\033[4;32mAll tests completed - program didn't crash!\033[0m")
