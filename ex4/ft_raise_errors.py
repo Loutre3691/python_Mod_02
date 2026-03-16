f7269ec1f98e0542524380fc9896f5a94835d68e
Flora De Checchi <flora.dechecchi@gmail.com>
2026-03-16 23:48:47 +0100

    ex

---
+++ ex4/ft_raise_errors.py
@@ -0,0 +1,17 @@
+def check_plant_health(plant_name, water_level, sunlight_hours) -> None:
+
+def test_plant_check() -> None:
+
+# ecrire ft check_plant_health(plant_name, water_level, sunlight_hours):
+#   verfier si le nom de la plante est valide (non vide)
+#   verfier si le niveau de l'eau est raisonnable (entre 1 et 10)
+#   verifier si la duree d ensoleillement est ok ( entre 2 et 12)
+#   genere des erreurs approprie avec message utile en cas d'erreur
+#   retourner un message sucess si tout est ok
+#
+# creer une ft test_plant_check():
+# tester avec de bonne valeurs
+# tester avec des mauvais noms de plantes (raise ValueError)
+# tester avec un mauvais nivveau d eau( raise ValueError)
+# tester avec un mauvais ensoleillement horaire ( raise ValueError)
+# detecter et traiter chaque erreur de maniere approrie
